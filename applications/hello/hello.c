39afc2aca0bf141085fd86275e38f762eea45964
Robin.Mueller <robin.mueller.m@gmail.com>
2020-11-23 01:17:52 +0100

    hello program made a bit more interesting

--- applications/hello/hello.c
+++ applications/hello/hello.c
@@ -4,11 +4,17 @@
 #include <rtems.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 rtems_task Init(
   rtems_task_argument ignored
 )
-{
-  printf( "\nHello World\n" );
-  exit( 0 );
+{  
+	printf( "\nHello World\n" );
+	for(;;) {
+		sleep(100);
+		printf("Alive!\n\r");
+	}
+	exit( 0 );
+
 }
