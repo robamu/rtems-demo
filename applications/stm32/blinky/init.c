f1112f67e10f21a9087a7ec859c88d5ef7ceb707
Robin.Mueller <robin.mueller.m@gmail.com>
2020-11-26 14:04:25 +0100

    all leds blinking now, eclipse proj improved

--- applications/stm32/blinky/init.c
+++ applications/stm32/blinky/init.c
@@ -6,36 +6,29 @@
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
  */
+#include "led.h"
+#include "stm32h7xx_nucleo.h"
 
 #include <stdio.h>
 #include <bsp.h>
 #include <rtems.h>
-#include "led.h"
 
-bool printoutEnabled = false;
 
-rtems_task Init(rtems_task_argument argument)
-{
-  rtems_status_code status;
+rtems_task Init(rtems_task_argument argument) {
+	rtems_status_code status;
 
-  ledInit();
+	BSP_LED_Init(LED1);
+	BSP_LED_Init(LED2);
+	BSP_LED_Init(LED3);
 
-  while (1) {
+	while (1) {
+		(void) rtems_task_wake_after( 1 * rtems_clock_get_ticks_per_second() );
+		BSP_LED_Toggle(LED1);
+		BSP_LED_Toggle(LED2);
+		BSP_LED_Toggle(LED3);
+	}
 
-    (void) rtems_task_wake_after( 1 * rtems_clock_get_ticks_per_second() );
-    ledOff();
-    if(printoutEnabled) {
-    	printf("LED Off\n\r");
-    }
-    (void) rtems_task_wake_after( 1 * rtems_clock_get_ticks_per_second() );
-    ledOn();
-    if(printoutEnabled) {
-    	printf("LED On\n\r");
-    }
-
-  }
-
-  status = rtems_task_delete( RTEMS_SELF );
+	status = rtems_task_delete( RTEMS_SELF );
 }
 
 /**************** START OF CONFIGURATION INFORMATION ****************/
