32b4922ae8d8d69cb11e8e01e4d8401b040c23af
Robin.Mueller <robin.mueller.m@gmail.com>
2020-11-26 14:17:55 +0100

    removed header

--- applications/stm32/blinky/init.c
+++ applications/stm32/blinky/init.c
@@ -1,11 +1,3 @@
-/*
- *  COPYRIGHT (c) 1989-2009.
- *  On-Line Applications Research Corporation (OAR).
- *
- *  The license and distribution terms for this file may be
- *  found in the file LICENSE in this distribution or at
- *  http://www.rtems.com/license/LICENSE.
- */
 #include "led.h"
 #include "stm32h7xx_nucleo.h"
 
