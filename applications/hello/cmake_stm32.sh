607cf9adad4c40ffc466f135d1bb4460a1e3d8da
Robin.Mueller <robin.mueller.m@gmail.com>
2020-12-11 15:50:50 +0100

    prefix test

--- applications/hello/cmake_stm32.sh
+++ applications/hello/cmake_stm32.sh
@@ -1,4 +1,3 @@
-# TODO: Only builds release, read second command line argument to parse command line arguments to
-# that is can build multiple configuratios?      
-cmake -DRTEMS_VERSION=6 -DRTEMS_TOOLS=$RTEMS_TOOLS -DRTEMS_BSP=arm/stm32h7 -DRTEMS_BSP_PATH=$RTEMS_ARM_STM32H7_BSP ..
+cmake -DRTEMS_VERSION=6 -DRTEMS_TOOLS=$RTEMS_TOOLS -DRTEMS_BSP=arm/stm32h7 \
+	-DRTEMS_BSP_PATH=$RTEMS_ARM_STM32H7_BSP -DRTEMS_PREFIX="."  ..
 cmake --build . $1
